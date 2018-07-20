07f300f0b9a6ab14b11027cfa29fc22dc5cf4696
MisterTakaashi <soapmctravich@gmail.com>
2018-07-20 20:32:29 +0200

    ajout variable environnement port

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+let port = process.env.SERV_PORT || 80;
+
+console.log("Ecoute sur le port " + port);
+server.listen(port);
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
